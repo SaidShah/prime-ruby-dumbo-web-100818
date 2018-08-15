db4937b2d13f7c2630f9f277eed8de1f340c53d8
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-15 22:18:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,5 +4,10 @@ def prime?(value_given)
   if(value_given<=1)
     false
     elsif(value_given<=3)
-  
+      true
+      elsif((value_given%2==0) || (value_given%3==0))
+        false
+      else
+        true
+      end
 end
